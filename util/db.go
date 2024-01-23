d3603b2b87c3636d02f02787e9654a3b1649e8e5
ahui2016 <ahui.2015@icloud.com>
2024-01-23 22:54:12 +0800

    rebuildSomeBuckets 未完成

--- util/db.go
+++ util/db.go
@@ -9,21 +9,21 @@ import (
 )
 
 var (
-	FilesBucket    = []byte("FilesBucket")
-	FilenameBucket = []byte("FilenameBucket")
-	ChecksumBucket = []byte("ChecksumBucket")
-	SizeBucket = []byte("SizeBucket")
-	TypeBucket = []byte("TypeBucket")
-	LikeBucket = []byte("LikeBucket")
-	LabelBucket = []byte("LabelBucket")
-	NotesBucket = []byte("NotesBucket")
-	KeywordsBucket = []byte("KeywordsBucket")
+	FilesBucket       = []byte("FilesBucket")
+	FilenameBucket    = []byte("FilenameBucket")
+	ChecksumBucket    = []byte("ChecksumBucket")
+	SizeBucket        = []byte("SizeBucket")
+	TypeBucket        = []byte("TypeBucket")
+	LikeBucket        = []byte("LikeBucket")
+	LabelBucket       = []byte("LabelBucket")
+	NotesBucket       = []byte("NotesBucket")
+	KeywordsBucket    = []byte("KeywordsBucket")
 	CollectionsBucket = []byte("CollectionsBucket")
-	AlbumsBucket = []byte("AlbumsBucket")
-	CTimeBucket = []byte("CTimeBucket")
-	UTimeBucket = []byte("UTimeBucket")
-	CheckedBucket = []byte("CheckedBucket")
-	DamagedBucket = []byte("DamagedBucket")
+	AlbumsBucket      = []byte("AlbumsBucket")
+	CTimeBucket       = []byte("CTimeBucket")
+	UTimeBucket       = []byte("UTimeBucket")
+	CheckedBucket     = []byte("CheckedBucket")
+	DamagedBucket     = []byte("DamagedBucket")
 )
 
 var buckets = [][]byte{
@@ -65,6 +65,13 @@ func createBuckets(db *bolt.DB) error {
 	})
 }
 
+func PutToBucket(key []byte, value []byte, b *bolt.Bucket) error {
+	if KeyExistsInBucket(key, b) {
+		return fmt.Errorf("key exists: %s", key)
+	}
+	return b.Put(key, value)
+}
+
 func bucketPutJson(k string, v any, b *bolt.Bucket) error {
 	data, err := json.Marshal(v)
 	if err != nil {
@@ -99,29 +106,6 @@ func AddFilesToDB(files []FileAndMeta, db *bolt.DB) error {
 		}
 		return nil
 	})
-}
-
-func putChecksum(f *File, b *bolt.Bucket) error {
-	ids := bucketGetStrSlice(f.Checksum, b)
-	if ids != nil {
-		ids = append(ids, f.ID)
-		return bucketPutJson(f.Checksum, ids, b)
-	}
-	return bucketPutJson(f.Checksum, []string{f.ID}, b)
-}
-
-func rebuildChecksumBucket(files []*File, tx *bolt.Tx) error {
-	b := tx.Bucket(ChecksumBucket)
-	for _, f := range files {
-		
-	}
-}
-
-func PutToBucket(key []byte, value []byte, b *bolt.Bucket) error {
-	if KeyExistsInBucket(key, b) {
-		return fmt.Errorf("key exists: %s", key)
-	}
-	return b.Put(key, value)
 }
 
 func DeleteInDB(ids []string, db *bolt.DB) error {
@@ -186,4 +170,79 @@ func FilesExistInDB(files []*File, db *bolt.DB) (existFiles []*File) {
 
 func KeyExistsInBucket(key []byte, b *bolt.Bucket) bool {
 	return b.Get(key) != nil
+}
+
+func reCreateBucket(name []byte, tx *bolt.Tx) (*bolt.Bucket, error) {
+	if err := tx.DeleteBucket(name); err != nil {
+		return nil, err
+	}
+	return tx.CreateBucket(name)
+}
+
+func rebuildSomeBuckets(files []*File, tx *bolt.Tx) error {
+	csumBuc := reCreateBucket(ChecksumBucket)
+	sizeBuc := reCreateBucket(SizeBucket)
+	typeBuc := reCreateBucket(TypeBucket)
+	likeBuc := reCreateBucket(LikeBucket)
+	labelBuc := reCreateBucket(LabelBucket)
+	notesBuc := reCreateBucket(NotesBucket)
+	kwBuc := reCreateBucket(KeywordsBucket)
+	collBuc := reCreateBucket(CollectionsBucket)
+	albumBuc := reCreateBucket(AlbumsBucket)
+	ctimeBuc := reCreateBucket(CTimeBucket)
+	utimeBuc := reCreateBucket(UTimeBucket)
+	checkBuc := reCreateBucket(CheckedBucket)
+	dmgBuc := reCreateBucket(DamagedBucket)
+
+	for _, f := range files {
+		e1 := putStrAndID(f.Checksum, f.ID, b)
+		e2 := putIntAndID(f.Size, f.ID, b)
+		e3 := putStrAndID(f.Type, f.ID, b)
+		e4 := putIntAndID(f.Like, f.ID, b)
+		e5 := putStrAndID(f.Label, f.ID, b)
+		e6 := putStrAndID(f.Notes, f.ID, b)
+		e7 := putSliceAndID(f.Keywords, f.ID, b)
+		e8 := putSliceAndID(f.Collections, f.ID, b)
+		e9 := putSliceAndID(f.Albums, f.ID, b)
+		e10 := putStrAndID(f.CTime, f.ID, b)
+		e11 := putStrAndID(f.UTime, f.ID, b)
+		e12 := putStrAndID(f.Checked, f.ID, b)
+		e13 := putIdAndBool(f.ID, f.Damaged, b)
+
+		if err := util.WrapErrors(e1, e2, e3, e4, e5, e6,
+			e7, e8, e9, e10, e11, e12, e13); err != nil {
+			return err
+		}
+	}
+}
+
+func putStrAndID(key, id string, b *bolt.Bucket) error {
+	ids := bucketGetStrSlice(key, b)
+	if ids != nil {
+		ids = append(ids, id)
+		return bucketPutJson(key, ids, b)
+	}
+	return bucketPutJson(key, []string{id}, b)
+}
+
+func putIntAndID(i int64, id string, b *bolt.Bucket) error {
+	key := strconv.FormatInt(i, 10)
+	return putKeyAndID(key, f.ID, b)
+
+}
+
+func putSliceAndID(s []string, id string, b *bolt.Bucket) error {
+	for _, item := range s {
+		if err := putStrAndID(item, id, b); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func putIdAndBool(id string, v bool, b *bolt.Bucket) error {
+	if v {
+		return b.Put([]byte(id), []byte("true"))
+	}
+	return nil
 }
