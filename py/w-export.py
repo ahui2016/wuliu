5230bf8b7f03e85ef16c60a6933dbef1b98e5d18
ahui2016 <ahui.2015@icloud.com>
2024-10-21 23:35:35 +0800

    def export_file_by_collection()

--- py/w-export.py
+++ py/w-export.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 from wuliu.const import *
 from wuliu.common import read_project_info
-from wuliu.db import open_db, db_select_by_id
+from wuliu.db import open_db, db_select_by_id, db_cache
 
 
 files_folder = Path(FILES)
@@ -21,8 +21,7 @@ def checkSizeLimit(size: int, info: dict):
         raise WuliuError(f"檔案體積({size_str}) 超過上限({info["ExportSizeLimit"]} MB)")
 
 
-def export_file(file_id: str, db: Conn, info: dict):
-    file = db_select_by_id(file_id, db)
+def export_file(file: dict, info: dict):
     checkSizeLimit(file[SIZE], info)
     src = files_folder.joinpath(file[FILENAME])
     dst = buffer_folder.joinpath(file[FILENAME])
@@ -31,6 +30,11 @@ def export_file(file_id: str, db: Conn, info: dict):
         return
     print(f"Export => {dst}")
     shutil.copyfile(src, dst)
+
+
+def export_file_by_id(file_id: str, db: Conn, info: dict):
+    file = db_select_by_id(file_id, db)
+    export_file(file, info)
 
 
 def export_meta(file_id: str, db: Conn):
@@ -44,9 +48,25 @@ def export_meta(file_id: str, db: Conn):
     shutil.copyfile(src, dst)
 
 
+def export_file_by_collection(coll: str, db: Conn, info: dict, danger: bool | None):
+    cache = db_cache(db)
+    files = [f for f in cache.values() if coll in f[COLLECTIONS]]
+    if not files:
+        print(f"找不到與 collection:{coll} 匹配的檔案")
+        return
+    if len(files) > 10 and not danger:
+        print(f"找到 {len(files)} 個檔案, 請使用 '-danger' 參數執行導出。")
+        return
+    for f in files:
+        export_file(f, info)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
+    parser.add_argument(
+        "-danger", action="store_true", help="allow dangerous operations"
+    )
     parser.add_argument("-file", type=str, help="specify a file ID and export the file")
     parser.add_argument(
         "-meta", type=str, help="specify a file ID and export the file's metadata(json)"
@@ -54,6 +74,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-id", type=str, help="specify a file ID and export the file and its metadata"
     )
+    parser.add_argument("-coll", type=str, help="export files by collection")
 
     args = parser.parse_args()
     info = read_project_info()
@@ -61,12 +82,14 @@ if __name__ == "__main__":
     db = open_db(Project_PY_DB)
 
     if args.file:
-        export_file(args.file, db, info)
+        export_file_by_id(args.file, db, info)
     elif args.meta:
         export_meta(args.meta, db)
     elif args.id:
-        export_file(args.id, db, info)
+        export_file_by_id(args.id, db, info)
         export_meta(args.id, db)
+    elif args.coll:
+        export_file_by_collection(args.coll, db, info, args.danger)
     else:
         parser.print_help()
 
