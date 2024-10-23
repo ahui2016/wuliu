efae7d8812615d824fef3166e85ba3db89b3c502
ahui2016 <ahui.2015@icloud.com>
2024-10-23 21:12:02 +0800

    Update w-rename.py

--- py/w-rename.py
+++ py/w-rename.py
@@ -4,8 +4,16 @@ from sqlite3 import Connection as Conn
 from pathlib import Path
 
 from wuliu.const import *
-from wuliu.common import read_project_info, json_load, type_by_filename, name_to_id, path_write_json
-from wuliu.db import open_db, db_cache, db_insert_file, db_delete_file, db_select_by_id
+from wuliu.common import (
+    read_project_info,
+    check_not_in_backup,
+    json_load,
+    type_by_filename,
+    name_to_id,
+    path_write_json,
+    print_err,
+)
+from wuliu.db import open_db, db_insert_file, db_delete_file, db_select_by_id
 
 
 files_folder = Path(FILES)
@@ -21,9 +29,12 @@ def check_exist(src: Path, dst: Path):
         raise WuliuError(f"file exists: {dst}")
 
 
-def check_filename(name):
-    if re.search(r'[\\\/\:\*\?\"\<\>\|]', name):
-        raise ValueError(r'檔案名稱不允許包含這些字符 \/:*?"<>|')
+def check_filename(old_name: str, new_name: str):
+    if old_name == new_name:
+        raise WuliuError("新檔案名與舊檔案名相同")
+    # if re.search(r'[\\\/\:\*\?\"\<\>\|]', new_name):
+    if re.search(r'[\\/:*?"<>|]', new_name):
+        raise WuliuError(r'檔案名稱不允許包含這些字符 \/:*?"<>|')
 
 
 def rename_file(old_name: str, new_name: str):
@@ -34,9 +45,9 @@ def rename_file(old_name: str, new_name: str):
     src.rename(dst)
 
 
-def rename_meta(old_name: str, new_name: str):
-    src = meta_folder.joinpath(old_name+".json")
-    dst = meta_folder.joinpath(new_name+".json")
+def rename_meta(old_name: str, new_name: str) -> dict:
+    src = meta_folder.joinpath(old_name + ".json")
+    dst = meta_folder.joinpath(new_name + ".json")
     print(f"Rename {src} => {dst}")
     check_exist(src, dst)
     meta = json_load(src)
@@ -45,31 +56,44 @@ def rename_meta(old_name: str, new_name: str):
     meta[TYPE] = type_by_filename(new_name)
     path_write_json(dst, meta)
     src.unlink()
+    return meta
 
 
-def rename_in_db(old_id: str, newfile: dict, db: Conn):
-    print(f"insert into db => {newfile[FILENAME]}")
-    db_insert_file(newfile, db)
-    print(f"delete from db => id:{old_id}")
+def rename_in_db(old_id: str, new_meta: dict, db: Conn):
+    print("Update database...")
+    db_insert_file(new_meta, db)
     db_delete_file(old_id, db)
+
+
+def rename(old_id: str, new_name: str, db: Conn):
+    try:
+        old_meta = db_select_by_id(old_id, db)
+        old_name = old_meta[FILENAME]
+        check_filename(old_name, new_name)
+    except WuliuError as err:
+        print_err(str(err))
+        return
+    rename_file(old_name, new_name)
+    new_meta = rename_meta(old_name, new_name)
+    rename_in_db(old_id, new_meta, db)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
+    parser.add_argument("-id", type=str, help="specify a file ID that exists")
     parser.add_argument("-name", type=str, help="set a new filename")
-    parser.add_argument(
-        "-meta", type=str, help="specify a file ID and export the file's metadata(json)"
-    )
-    parser.add_argument(
-        "-id", type=str, help="specify a file ID and export the file and its metadata"
-    )
-    parser.add_argument("-coll", type=str, help="export files by collection")
 
     args = parser.parse_args()
     info = read_project_info()
-    # check_not_in_backup(info)
+    check_not_in_backup(info)
 
-    old_file = db_select_by_id(old_id, db)
-
-    check_filename(args.name)
+    db = open_db(Project_PY_DB)
+    if args.id and (not args.name):
+        print_err("Required '-name'")
+    elif args.name and (not args.id):
+        print_err("Required '-id'")
+    elif args.id and args.name:
+        rename(args.id, args.name, db)
+    else:
+        parser.print_help()
+    db.close()
